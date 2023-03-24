770169bb72b578d9990592edc90042946092f656
lldel59 <112475267+lldel59@users.noreply.github.com>
2023-03-24 15:30:48 +0100

    Add files via upload

---
+++ read_user.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta charset="utf-8" />
+    	<title>Lecture user</title>
+    	<link href="style.css" rel="stylesheet" />
+	</head>
+
+	<body>
+    	<h1>Read users</h1>
+
+<?php
+    	foreach ($posts as $post) {
+    	?>
+        	<div class="news">
+            	<h3>
+                	<?php echo ($post['nom'].' '.($post['prenom'])); ?>
+            	</h3>
+            	<p>
+                	<?php
+                	// We display the post content.
+                	echo "</br>adresse : ".($post['adresse']);
+                    echo "</br>ville : ".($post['ville']);
+                    echo "</br>prat_id : ".($post['prat_id']);
+                	?>
+                	<br />
+                	<em><a href="?show=prat&id=<?=($post['prat_id'])?>"> Voir le praticien </a></em>
+            	</p>
+        	</div>
+    	<?php
+    	} // The end of the posts loop.
+    	?>
+	</body>
+</html>

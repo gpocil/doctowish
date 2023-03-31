3209fd0c27240d11f983de5d95191cb17f8fbc47
P.O Causs <112474708+gpocil@users.noreply.github.com>
2023-03-31 12:16:16 +0200

    Delete mvc directory

--- mvc/contr/read_user_contr.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include( "model/read_user_model.php");
-$posts = read_user($_GET['id']);
-include( "vues/read_user.php");
-
--- mvc/model/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function connexion(){
-try {
-	$database = new PDO('mysql:host=localhost;dbname=doctowish;charset=utf8', 'root', '');
-} catch(Exception $e) {
-	die('Erreur : '.$e->getMessage());
-}
-return $database;
-}

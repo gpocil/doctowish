4b2f336ad1954ab124b7d520faf33879e844f2f6
Az Iz Ko Ke <aziz.diarra@laposte.net>
2023-04-14 14:31:33 +0200

    modified:   assets/js/formPraticien.js 	modified:   controleur/Controleur_inscription_praticien.php 	modified:   controleur/Controleur_inscription_user.php

--- controleur/Controleur_inscription_praticien.php
+++ controleur/Controleur_inscription_praticien.php
@@ -3,7 +3,7 @@ require("vue/inscription_praticien.php");
 require("modele/modele_inscription_prat.php");
 if (isset($_POST["action"]) && !empty($_POST["action"])) {
     inscription_prat();
-echo "succeed";
+    echo "succeed";
 }
 ?>
 

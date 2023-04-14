9267baeb704e59820d2d0e47df7793ed4fb947b4
Az Iz Ko Ke <aziz.diarra@laposte.net>
2023-04-14 15:17:52 +0200

    HJJHJ

--- controleur/Controleur_inscription_user.php
+++ controleur/Controleur_inscription_user.php
@@ -7,13 +7,3 @@ require("modele/modele_insciption_user.php");
 if (isset($_POST["action"]) && !empty($_POST["action"])) {
     inscription_user();
 }
-//------------------TODO : Contraintes email etc---------------
-=======
-<?php
-session_set_cookie_params(0);
-session_start();
-require("vue/inscription_user.php");
-require("modele/modele_insciption_user.php");
-inscription_user();
-//------------------TODO : Contraintes email etc---------------
->>>>>>> 45ac4cc9c3994f59eae22ee35c2f8ec0bdc121a1

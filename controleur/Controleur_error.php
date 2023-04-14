5a578cd279fffcaba0b3fbc883ffb31839a35dc1
P.O Causs <112474708+gpocil@users.noreply.github.com>
2023-04-14 15:29:28 +0200

    Delete controleur directory

--- controleur/Controleur_error.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-require("vue/error.php");
-?>
--- controleur/Controleur_inscription_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_set_cookie_params(0);
-session_start();
-require("vue/inscription_user.php");
-require("modele/modele_insciption_user.php");
-if (isset($_POST["action"]) && !empty($_POST["action"])) {
-    inscription_user();
-}
-//------------------TODO : Contraintes email etc---------------

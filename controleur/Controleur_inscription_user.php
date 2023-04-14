14738537ebe89a7d9cc1c82148d398716717f872
Az Iz Ko Ke <aziz.diarra@laposte.net>
2023-04-14 15:25:35 +0200

    LIGHT

--- controleur/Controleur_inscription_user.php
+++ controleur/Controleur_inscription_user.php
@@ -5,9 +5,4 @@ require("vue/inscription_user.php");
 require("modele/modele_insciption_user.php");
 if (isset($_POST["action"]) && !empty($_POST["action"])) {
     inscription_user();
-<<<<<<< HEAD
 }
-=======
-}
-//------------------TODO : Contraintes email etc---------------
->>>>>>> a14bbb40d243560312edbdcb8a52bd77cc53801c
